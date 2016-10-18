b9b3f01ac117b889dc5a0dd1eb5ed040304b147e
sheharyar12 <syar0052@gmail.com>
2016-10-18 00:32:37 -0400

    Spring Annotation assuming spring libraries already existing and package name is com.sheharyar.springdemo

---
+++ AnnotationDemoApp.java
@@ -0,0 +1,18 @@
+package com.sheharyar.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationDemoApp {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("applicationContext.xml");
+
+		Coach theCoach = context.getBean("thatSillyCoach",Coach.class);
+		
+		System.out.println(theCoach.getDailyWorkout());
+		
+		context.close();
+	}
+
+}
